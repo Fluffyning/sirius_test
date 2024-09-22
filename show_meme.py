c37f95ef5573d7289fa3e5e72f17ed382ae612e2
Sanya <sasha56koles09@gmail.com>
2024-09-22 23:54:06 +0300

    error

--- show_meme.py
+++ show_meme.py
@@ -2,7 +2,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 if __name__=='__main__':
-    img = Image.open('memes/meme_2.png')
+    img = Image.open('memes/meme_2sdfgfd.png')
 
     plt.imshow(img)
     plt.axis('off')
