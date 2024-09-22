936441a2413f0d9adc720f2955a5eb49ac3971c9
Sanya <sasha56koles09@gmail.com>
2024-09-22 23:52:49 +0300

    meme 2

--- show_meme.py
+++ show_meme.py
@@ -2,7 +2,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 if __name__=='__main__':
-    img = Image.open('memes/meme_1.png')
+    img = Image.open('memes/meme_2.png')
 
     plt.imshow(img)
     plt.axis('off')
